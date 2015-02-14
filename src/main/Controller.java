adeed73366caf5a7cb94bff7e6e166a2fb22ea42
Ryoto Tomioka <tommy.ryoto93@gmail.com>
2015-02-15 01:30:45 +0900

    keyboard code

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -11,10 +11,127 @@ public class Controller {
     private Button key1;
 
     @FXML
+    private Button key2;
+
+    @FXML
+    private Button key0;
+
+    @FXML
+    private Button key5;
+
+    @FXML
+    private Button key6;
+
+    @FXML
+    private Button key3;
+
+    @FXML
+    private Button key4;
+
+    @FXML
+    private Button key9;
+
+    @FXML
     private Button keyJapan;
 
     @FXML
+    private Button key7;
+
+    @FXML
+    private Button keyMinus;
+
+    @FXML
+    private Button key8;
+
+    @FXML
+    private Button keyCoron;
+
+    @FXML
+    private Button keyYen;
+
+    @FXML
+    private Button keyQ;
+
+    @FXML
+    private Button keyR;
+
+    @FXML
+    private Button keyAt;
+
+    @FXML
+    private Button keyHat;
+
+    @FXML
+    private Button keyO;
+
+    @FXML
+    private Button keyP;
+
+    @FXML
+    private Button keyBS;
+
+    @FXML
+    private Button keyU;
+
+    @FXML
+    private Button keyS;
+
+    @FXML
+    private Button keyRBracket;
+
+    @FXML
+    private Button keyT;
+
+    @FXML
+    private Button keySemiCoron;
+
+    @FXML
+    private Button keyTab;
+
+    @FXML
+    private Button keyY;
+
+    @FXML
+    private Button keyW;
+
+    @FXML
+    private Button keyA;
+
+    @FXML
+    private Button keyLBracket;
+
+    @FXML
+    private Button keyE;
+
+    @FXML
+    private Button keyF;
+
+    @FXML
+    private Button keyD;
+
+    @FXML
+    private Button keyI;
+
+    @FXML
+    private Button keyJ;
+
+    @FXML
+    private Button keyG;
+
+    @FXML
+    private Button keyH;
+
+    @FXML
     private MenuItem menuItemClose;
+
+    @FXML
+    private Button keyCaps;
+
+    @FXML
+    private Button keyK;
+
+    @FXML
+    private Button keyL;
 
     @FXML
     void close(ActionEvent event) {
