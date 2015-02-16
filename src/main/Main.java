46b12d58c3e9dc4fe266099f9a1ee73bf739ccff
Ryoto Tomioka <tommy.ryoto93@gmail.com>
2015-02-16 19:57:45 +0900

    clean up

--- src/main/Main.java
+++ src/main/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws IOException{
-        new TypingWindowFrame(stage);
+        new TypingGame(stage);
         stage.show();
     }
 }
---
+++ src/main/TypingGame.java
@@ -0,0 +1,69 @@
+package main;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Random;
+
+/**
+ * Created by Ryoto on 2/9/2015.
+ */
+public class TypingGame {
+    private TextField inputBox;
+
+    public TypingGame(Stage stage) throws IOException{
+
+        Parent root = FXMLLoader.load(getClass().getResource("TypingWindowFrame.fxml"));
+        root.setOnKeyPressed(new TypingController());
+
+        stage.setTitle("Typing Trainer");
+        stage.setScene(new Scene(root)  );
+
+    }
+
+    public void homePositionTrain() {
+        int v, f;
+        Random random = new Random();
+
+        for(int i=0; i<10; i++) {
+            for(int j=0; j<10; j++) {
+                if(i<5) {
+                    f = random.nextInt(8);
+                    switch(f) {
+                        case 1:
+
+                            break;
+                        case 2:
+
+                            break;
+                        case 3:
+
+                            break;
+                        case 4:
+
+                            break;
+                        case 5:
+
+                            break;
+                        case 6:
+
+                            break;
+                        case 7:
+
+                            break;
+                        case 8:
+
+                            break;
+                    }
+                } else {
+
+                }
+            }
+        }
+    }
+}
