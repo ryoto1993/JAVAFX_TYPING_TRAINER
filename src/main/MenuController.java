8307a47c4501f193f6a295620a34904754c23ac3
Ryoto Tomioka <tommy.ryoto93@gmail.com>
2015-02-19 22:17:13 +0900

    add menu controller

---
+++ src/main/MenuController.java
@@ -0,0 +1,23 @@
+package main;
+
+/**
+ * Created by Ryoto on 2/19/2015.
+ */
+
+import javafx.fxml.FXML;
+import javafx.scene.shape.Ellipse;
+
+public class MenuController {
+    @FXML
+    private Ellipse homePositionTrainerButton;
+
+    @FXML
+    private Ellipse exitButton;
+
+    @FXML
+    private Ellipse homePositionTrainerKanaButton;
+
+    @FXML
+    private Ellipse moguraTatakiButton;
+
+}
