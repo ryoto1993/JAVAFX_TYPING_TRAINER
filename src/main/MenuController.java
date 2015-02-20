5c6e43ebfbe30a8f1b2d3a24e7e58fecf6cae58e
Ryoto Tomioka <tommy.ryoto93@gmail.com>
2015-02-20 18:44:51 +0900

    menu listener added

--- src/main/MenuController.java
+++ src/main/MenuController.java
@@ -4,7 +4,7 @@ package main;
  * Created by Ryoto on 2/19/2015.
  */
 
-import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
@@ -24,8 +24,17 @@ public class MenuController {
     private Ellipse moguraTatakiButton;
 
     @FXML
-    public void exit() {
-        System.exit(2);
+    public void enter(Event e) {
+        Object tmp = e.getSource();
+        if(tmp.equals(homePositionTrainerButton)) {
+
+        } else if(tmp.equals(homePositionTrainerKanaButton)) {
+
+        } else if(tmp.equals(moguraTatakiButton)) {
+
+        } else if(tmp.equals(exitButton)) {
+            System.exit(2);
+        }
     }
 
     @FXML
